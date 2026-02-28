84ef18d0d10016fde07cfdc00a82f7f21ae16ad0
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-02-28 12:03:04 +0530

    removed .env file

--- create-post/frontend/src/pages/Feed.jsx
+++ create-post/frontend/src/pages/Feed.jsx
@@ -4,17 +4,18 @@ import axios from 'axios'
 const Feed = () => {
 
     const [posts, setPosts] = useState([
-        {
-            _id: 1,
-            image: 'https://images.unsplash.com/photo-1542224566-6e85f2e6772f?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            caption: 'Beautiful sunset!',
-        }
+        // {
+        //     _id: 1,
+        //     image: 'https://images.unsplash.com/photo-1542224566-6e85f2e6772f?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        //     caption: 'Beautiful sunset!',
+        // }
     ])
 
     useEffect(() => {
         axios.get('http://localhost:3000/posts')
-            .then(res => {
-                setPosts(res.data)
+            .then((res) => {
+                setPosts(res.data.posts);
+                console.log(res.data.posts)
             })
             .catch(err => {
                 console.log(err)
